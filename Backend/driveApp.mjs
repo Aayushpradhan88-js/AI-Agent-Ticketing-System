3dbfe990cf47ec2a4081a44e2e55e59d984e1b5a
Ayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-12-30 16:55:54 +0545

    refactor: remove unused user controller and rename userDB to db

--- Backend/driveApp.mjs
+++ Backend/driveApp.mjs
@@ -2,32 +2,36 @@ import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 
-import mongoDb from "./db/userDB.mjs";
+
+dotenv.config();
+app.use(cors());
+const app = express();
+
+//imported files
 import userSchema from "./models/user.model.mjs";
 import userFilesSchema from "./models/files.model.mjs";
 import router from "./routes/userApi.route.mjs";
+import mongoDb from "./db/db.mjs";
 
-const app = express();
-dotenv.config();
-app.use(cors());
 userSchema();
 userFilesSchema();
 router;
 mongoDb();
 
-// set up middlewares
+//middlewares
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 app.use("/user", router);
-// app.use('/user', require('./routes/user.route.js'));
+
 
 //Port
 const PORT=3000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`);
-} )
+})
 
 export default app;
--- Backend/routes/userApi.route.mjs
+++ Backend/routes/userApi.route.mjs
@@ -1,9 +1,1 @@
-import express from 'express'
-import multer from 'multer'
-import userModel from '../models/user.model.mjs'
-import userFiles from '../models/files.model.mjs'
-import jwt from 'jsonwebtoken';
-import validato, { validationResult } from 'express-validator';
-
-const router = express.Router();
-
+import express from 'express';
