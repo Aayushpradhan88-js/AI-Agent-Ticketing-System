174842c25659cb7f8dd51a099e5ad74decf026ca
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2025-06-17 12:13:16 +0545

    initializing tailwindcss to storage

---
+++ frontend/App/src/Pages/AppLandingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AppLandingPage = () => {
+  return (
+    <div>CREATE YOUR OWN FILE MANAGEMENT </div>
+  )
+}
---
+++ frontend/App/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    < >
+      <div>
+        
+      </div>
+
+
+    <div> Welcome to your personal storage</div>  <div>
+        
+    </>
+  )
+}
