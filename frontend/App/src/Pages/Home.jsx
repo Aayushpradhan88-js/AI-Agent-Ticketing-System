3358bb14bec93953e1b6cfa98a21bb3ac18d8020
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2025-06-17 12:14:37 +0545

    writing dahbord ui

--- frontend/App/src/Pages/Home.jsx
+++ frontend/App/src/Pages/Home.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    < >
-      <div>
-        
-      </div>
-
-
-    <div> Welcome to your personal storage</div>  <div>
+    <>
+      <div>welcome</div>
         
     </>
   )
