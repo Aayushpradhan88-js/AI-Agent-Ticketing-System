176111eeb8af1beaa0ec3671dc8048ffd87ee330
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2025-06-19 21:04:01 +0545

    cofiguring cloudinary

--- frontend/App/src/Pages/Home.jsx
+++ frontend/App/src/Pages/Home.jsx
@@ -3,6 +3,26 @@ import React from 'react'
 export const Home = () => {
   return (
     <>
+      <div>
+        {/* Header */}
+        <div>
+             Personal Storage
+        </div>
+
+        <div className='bg-black'>
+          search files
+        </div>
+
+        <div className='bg-blue-700 rounded-b-full '>
+          <button>upload</button>
+        </div>
+     
+     <div>
+      user profile
+     </div>
+
+      </div>
+
       <div>welcome</div>
         
     </>
