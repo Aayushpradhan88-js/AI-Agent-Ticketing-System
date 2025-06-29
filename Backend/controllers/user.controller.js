6a1d2cd43203ce61a0ece86a7c9413b33cd18e13
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2025-06-29 19:27:50 +0545

    refactor: improve code formatting and consistency in user controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,21 +1,21 @@
-import {User} from '../models/user.models.js';
+import { User } from '../models/user.models.js';
 
 import { ApiResponse } from '../utils/ApiResponse.js';
-import {ApiError} from '../utils/ApiError.js'
+import { ApiError } from '../utils/ApiError.js'
 
-const generateAccessTokenAndRefreshToken = async(userId) => {
+const generateAccessTokenAndRefreshToken = async (userId) => {
 
     try {
         const user = await User.findById(userId);
         const accessToken = user.generateAccessToken();
         const refreshToken = user.generateRefreshToken();
-    
+
         user.refreshToken = refreshToken;
-        await user.save({validataBeforeSave: false})
-    
-        return {accessToken, refreshToken};
-    } 
-    
+        await user.save({ validataBeforeSave: false })
+
+        return { accessToken, refreshToken };
+    }
+
     catch (error) {
         throw new ApiError(500, "UNABLE TO GENERATE ACCESS & REFRESH TOKEN");
     };
@@ -23,22 +23,20 @@ const generateAccessTokenAndRefreshToken = async(userId) => {
 }
 
 
-const signUpUser = async(req, res) => {
-    const {username, fullname, email, password} = req.body;
-    console.log(username) 
-    console.log("running")
-     console.log(process.env.MONGO_URL)
-    if(
-        [username, fullname, email, password].some((field) => field?.trim() === '')   
-    ){
+const signUpUser = async (req, res) => {
+    const { username, fullname, email, password } = req.body;
+    console.log(username)
+    if (
+        [username, fullname, email, password].some((field) => field?.trim() === '')
+    ) {
         throw new ApiError(401, "ALL FIELDS ARE REQUIRED!!!")
     };
 
     const isExisted = await User.find({
-        $or: [{username}, {email}]
+        $or: [{ username }, { email }]
     });
 
-    if(!isExisted) throw new ApiError(401, "USERNAME OR EMAIL IS ALREADY TAKEN");
+    if (!isExisted) throw new ApiError(401, "USERNAME OR EMAIL IS ALREADY TAKEN");
 
     const user = await User.create({
         username,
@@ -50,44 +48,39 @@ const signUpUser = async(req, res) => {
     console.log(user);
     console.log("running")
 
-    if(!user) throw new ApiError(500, "SERVER OR DATABASE INTERNAL ERROR");
+    if (!user) throw new ApiError(500, "SERVER OR DATABASE INTERNAL ERROR");
 
-     const {refreshToken} = await generateAccessTokenAndRefreshToken(user._id);
-    const createdUser = await User.findById(user._id).select("-password -refreshToken"); 
+    const { refreshToken } = await generateAccessTokenAndRefreshToken(user._id);
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
 
-       console.log("running token")
-
-        return res
+    return res
         .status(201)
         .cookie('refreshToken', refreshToken, {
-            httpOnly: true, 
+            httpOnly: true,
             secure: true,
             sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60* 1000 //-----7days in ms-----//
+            maxAge: 7 * 24 * 60 * 60 * 1000 //-----7days in ms-----//
         })
         .json(
             new ApiResponse(
-                200, 
-                createdUser, 
+                200,
+                createdUser,
                 "User registered Successfully"
             )
         )
-
-        //  console.log("running token")
-    
 }
 
-const signInUser = async(req, res) => {
-    const {email, password} = req.body;
+const signInUser = async (req, res) => {
+    const { email, password } = req.body;
 
-    if(
-        [email, password].some((fields) => fields?.trim() === 'undefined') 
+    if (
+        [email, password].some((fields) => fields?.trim() === 'undefined')
     ) {
         throw new ApiError(401, "ALL FIELDS ARE REQUIRED");
     };
 }
 
-const logoutUser = async(req, res) =>{
+const logoutUser = async (req, res) => {
 
 }
 
