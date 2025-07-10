f3ab2e3a6a1023ad5dfe4d9ab5f21c5f1e05bc2a
Aayushpradhan88-js <AAYUSHPRADHAN996@outlook.com>
2025-07-10 21:15:19 +0545

    authentication logic

--- google-drive/components/AuthForm.tsx
+++ google-drive/components/AuthForm.tsx
@@ -1,5 +1,45 @@
 "use Client"
 
 import { useState } from "react";
+import {useForm} from "react-hook-form"
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
+import { object } from "zod/v4-mini";
+
+type FormType = "sign-in" | "sign-up";
+
+const AuthFormReolver = (formType : FormType) => {
+    return z.object({
+        email: z.string().email(),
+        fullname: 
+          formType === 'sign-up' ? z.string().min(2).max(20) : z.string().optional()
+    });
+}
+
+function AuthForm({type}: {formType: FormType}) {
+    const[isLoading, setIsLoading] = useState(false);
+    const[errorMessage, setErrorMessage] = useState("");
+    const[accountId, setAccountId] = useState(null);
+
+    const formSchema = AuthFormReolver(type);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+            resolver: zodResolver(formSchema),
+            defaultValues: {
+                email: "",
+                fullname: ""
+            }
+    });
+
+    const formSubmit = async (values: z.inferor<typeof formSchema>) => {
+        setIsLoading(true)
+        setErrorMessage("");
+
+        const user = type === "signup" ? await createAccount({
+            fullname: values.fullname | "",
+            email: values.email
+        }) : await SignInUser({ email: values.email})
+    }
+}
+
+export default AuthForm;
