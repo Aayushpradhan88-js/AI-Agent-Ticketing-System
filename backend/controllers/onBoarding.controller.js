7628921ad3006618fe8463122cbda82472861cbc
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-10-17 16:51:26 +0545

    add onboarding backend

--- backend/controllers/onBoarding.controller.js
+++ backend/controllers/onBoarding.controller.js
@@ -1,20 +1,48 @@
 import { User } from "../models/user.models";
 
-export const onBoarding = async (req, res, next) =>{
-    const {userType, experience, interest, goals} = req.body;
+export const onBoarding = async (req, res, next) => {
+    const { userType, experience, interest, goals } = req.body;
 
     try {
         let role;
 
-        if(userType === 'student'){
+        if (userType === 'student') {
             role = 'student'
-        } else if (userType === 'moderator'){
+            console.log("student onboarding")
+        } else if (userType === 'moderator') {
             role = 'moderator'
+            console.log("moderator onboarding")
         } else if (userType === 'admin') {
             role = 'admin'
+            console.log("admin onboarding");
         }
 
-        
+        const user = await User.findById(
+            req.user._id,
+            {
+                role,
+                onBoardingCompleted: true,
+                onBoardingData: {
+                    experience,
+                    goals,
+                    interest,
+                }
+            }
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res
+        .status(200)
+        .json(
+            {
+                message: "onboarding completed successfully",
+                user
+            }
+        )
+
     } catch (error) {
         console.log(error.message)
     }
