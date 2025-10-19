efe44f377d95c32eaef171c7457a52d936b75d83
Ayush Pradhan <AAYUSHPRADHAN996@outlook.com>
2025-10-19 17:01:34 +0545

    making interface for statemanagement

--- frontend-agent/src/components/OnBoarding.tsx
+++ frontend-agent/src/components/OnBoarding.tsx
@@ -13,11 +13,88 @@ interface Question {
 }
 
 interface OnboardingState {
-   //---state---//
-   step: Step;
-   userType: UserType | '';
-   currentQuestionIndex: number;
-   answers: Record<string, string>;
-   error: string;
-   isSumbitting: boolean
-}
+    //---STATE---//
+    step: Step;
+    userType: UserType | '';
+    currentQuestionIndex: number;
+    answers: Record<string, string>; //-----Record<KeyType, ValueType> = it is like a object-----//
+    error: string;
+    isSubmitting: boolean;
+
+    //---ACTIONS---//
+    setStep: (step: Step) => void;
+    setUserType: (type: UserType) => void;
+    setCurrentQuestionIndex: (index: number) => void;
+    setAnswers: (answers: Record<string, string>) => void;
+    setError: (error: string) => void;
+    setIsSubmitting: (isSubmitting: boolean) => void;
+
+    //---COMPUTED ACTIONS---//
+    resetState: () => void;
+    selectAnswer: (field: string, option: string) => void;
+    goToNextQuestion: () => void;
+    goToPreviousQuestion: () => void;
+};
+
+const useOnboardingStore = create<OnboardingState>((set) => {
+    step: 'userType';
+    userType: '';
+    currentQuestionIndex: 0;
+    answers: { };
+    error: '';
+    isSumbitting: false;
+
+    setStep: (step: Step) => set({ step });
+    setUserType: (userType: UserType) => set({ userType });
+    setCurrentQuestionIndex: (index: number) => set({ currentQuestionIndex: index })
+    setAnswers: (answers: Record<string, string>) => set({ answers });
+    setError: (error: string) => set({ error });
+    setIsSubmitting: (isSubmitting: boolean) => set({ isSubmitting })
+
+    resetState: () => set({
+        step: 'questions',
+        currentQuestionIndex: 0,
+        answers: {},
+        error: '',
+    });
+
+    selectAnswer: (field: string, option: string) => set((state) => ({
+        answers: { ...state.answers, [field]: option }
+    }));
+
+    goToNextQuestion: () => set((state) => ({
+        currentQuestionIndex: state.currentQuestionIndex + 1,
+        error: '',
+    }));
+
+    goToPreviousQuestion: () => set((state) => ({
+        currentQuestionIndex: state.currentQuestionIndex - 1,
+        error: ''
+    }))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
