d2d23db0b7177c1d1fb6ccb3a5527d0a0686a381
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-10-29 14:23:40 +0545

    ...

--- backend/controllers/onBoarding.controller.js
+++ backend/controllers/onBoarding.controller.js
@@ -2,6 +2,22 @@ import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.utils.js";
 
 export const onBoardingController = async (req, res,) => {
+
+    /*
+    Algorithm
+    1. first we extract the userid from the request of client
+    2. Then we extract userType and answers fromt he request
+    3. applied try catch block: catch block - writing error if user makes error & try block - In try i have validate user with each portion and update user saved in database
+    4. Try block destructuring and defining the types:
+       -- 4a. data we have (role, userType{stu., mod., adm.}, answers)
+       -- 4b. 1st we validate the frontend request 
+              -- 4b1. Is user Type is comming is not ApiError will passed with 400 status
+              -- 4b2. Check the type of useType is it stu., mod., adm.
+              -- 4c3. Update User - It require to call db where we over-write the data 
+                                  - for updation we make '{ }' & put role, onBoardingData, onBoardingStatus
+                                  - 
+
+    */
     const userId = req.user._id;
     const { userType, answers } = req.body;
 
@@ -30,8 +46,8 @@ export const onBoardingController = async (req, res,) => {
             userId,
             {
                 role: userType,
-                onBoardingCompleted: new Date(),
-                onBoardingData: answers
+                onBoardingData: answers,
+                onBoardingCompleted: new Date()
             }, { new: true, runValidators: true }
         ).select('-password')
 
